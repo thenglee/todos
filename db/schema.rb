c5fe9b644bac10af2866fda2b008cee86190c9f4
Tan Theng Lee <thengleetan@gmail.com>
2019-09-15 16:51:31 +0800

    Allow user to mark todo as completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_15_051041) do
+ActiveRecord::Schema.define(version: 2019_09_15_083933) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
+    t.datetime "completed_at"
   end
 
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
+  describe '#completed?' do
+    it 'returns true if completed_at is set' do
+      todo = Todo.new(completed_at: Time.current)
+
+      expect(todo).to be_completed
+    end
+
+    it 'returns false if completed at is nil' do
+      todo = Todo.new(completed_at: nil)
+
+      expect(todo).not_to be_completed
+    end
+  end
 end
